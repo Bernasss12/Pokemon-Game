eeab2c7411d39d4f0a1b1c7821cf73c7e144b8d7
Bernardo <tudo.pt.minecraft@gmail.com>
2018-09-03 23:48:22 +0100

    libGDX start

---
+++ desktop/src/dev/bernasss12/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.bernasss12.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.bernasss12.PkmnMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PkmnMain(), config);
+	}
+}
